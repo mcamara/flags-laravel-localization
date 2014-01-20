3309aa3b1c42621cf914d701f9e96116bb060b5f
Marc Cámara <mcamara88@gmail.com>
2014-01-20 17:54:36 +0100

    laravel installed

---
+++ app/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::group(array('prefix' => LaravelLocalization::setLanguage()), function()
+    {
+        /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+        Route::get('/', function()
+        {
+            return View::make('hello');
+        });
+
+        Route::get('test',function(){
+            return View::make('test');
+        });
+    });
